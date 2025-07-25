1fb26ca3f12e4ea35807e371a70510eb4aae460b
Rubber Yunez <61928963+ryunezm@users.noreply.github.com>
2025-07-25 17:40:29 -0500

    Update body.ts

--- src/app/body/body.ts
+++ src/app/body/body.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  imports: [FormsModule, MatButtonModule, MatFormField, MatIconModule, MatInputModule],
   templateUrl: './body.html',
   styleUrl: './body.scss'
 })
 export class Body {
-
+  value = '';
 }
