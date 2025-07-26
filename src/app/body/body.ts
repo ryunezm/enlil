2adec4fd08c8f5ef8733e236f5191e7fcf73fb66
Rubber Yunez <61928963+ryunezm@users.noreply.github.com>
2025-07-26 13:08:21 -0500

    Update body.ts

--- src/app/body/body.ts
+++ src/app/body/body.ts
@@ -3,14 +3,20 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
-import { MatFormField } from '@angular/material/form-field';
+import {MatFormField} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-body',
-  imports: [FormsModule, MatButtonModule, MatFormField, MatIconModule, MatInputModule],
+  imports: [FormsModule, MatButtonModule, MatCardModule, MatChipsModule, MatFormField, MatIconModule, MatInputModule],
   templateUrl: './body.html',
   styleUrl: './body.scss'
 })
 export class Body {
   value = '';
+
+  searchCity() {
+    console.log('Implementar para buscar ciudades');
+  }
 }
