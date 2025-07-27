c96d95609f582f9e4cf6e73f44217bf9f767218b
Rubber Yunez <61928963+ryunezm@users.noreply.github.com>
2025-07-27 12:11:16 -0500

    Async approach
    So list of city render when ready.

--- src/app/body/body.ts
+++ src/app/body/body.ts
@@ -5,28 +5,40 @@ import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatListModule} from '@angular/material/list';
 import {CityResult, Nominatim} from '../services/nominatim';
+import {Observable, of} from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-body',
   imports: [
+    AsyncPipe,
     FormsModule,
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ],
   templateUrl: './body.html',
   styleUrl: './body.scss'
 })
 export class Body {
   value = '';
+  results$: Observable< CityResult[]> = of([]);
+  selectedCity?: CityResult;
 
   constructor(private nominatim: Nominatim) {}
 
   searchCity() {
     if(!this.value.trim()) return;
-    this.nominatim.searchFlexible(this.value).subscribe((results: CityResult[])=> {console.log(results)});
+    this.results$ = this.nominatim.searchFlexible(this.value);
+  }
+
+  selectCity(city: CityResult) {
+    this.selectedCity = city;
+    console.log('Selected city:', this.selectedCity);
   }
 }
