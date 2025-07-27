11c554eceddec740216326470559fb496006d809
Rubber Yunez <61928963+ryunezm@users.noreply.github.com>
2025-07-27 15:51:36 -0500

    Update body.ts

--- src/app/body/body.ts
+++ src/app/body/body.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {AsyncPipe, DatePipe, DecimalPipe, TitleCasePipe} from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
@@ -7,30 +8,34 @@ import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatListModule} from '@angular/material/list';
 import {CityResult, Nominatim} from '../services/nominatim';
+import {Openweather, WeatherResponse} from '../services/openweather';
 import {Observable, of} from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-body',
   imports: [
     AsyncPipe,
+    DatePipe,
+    DecimalPipe,
     FormsModule,
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
     MatIconModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    TitleCasePipe
   ],
   templateUrl: './body.html',
   styleUrl: './body.scss'
 })
 export class Body {
   value = '';
-  results$: Observable< CityResult[]> = of([]);
   selectedCity?: CityResult;
+  results$: Observable<CityResult[]> = of([]);
+  weatherResponse$: Observable<WeatherResponse> = of();
 
-  constructor(private nominatim: Nominatim) {}
+  constructor(private nominatim: Nominatim, private openweather: Openweather) {}
 
   searchCity() {
     if(!this.value.trim()) return;
@@ -40,5 +45,15 @@ export class Body {
   selectCity(city: CityResult) {
     this.selectedCity = city;
     console.log('Selected city:', this.selectedCity);
+    this.getWeatherFromSelectedCity();
   }
+
+  getWeatherFromSelectedCity() {
+    if(!this.selectedCity?.lat || !this.selectedCity?.lon) return;
+
+    const latitude = parseFloat(this.selectedCity?.lat);
+    const longitude = parseFloat(this.selectedCity?.lon);  
+    this.weatherResponse$ = this.openweather.getCurrentWeatherByLatLon(latitude, longitude);
+    console.log(this.weatherResponse$);
+  }  
 }
