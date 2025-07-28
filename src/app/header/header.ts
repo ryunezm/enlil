ce3667718ef7413b722536029dd562bf07b583d1
Rubber Yunez <61928963+ryunezm@users.noreply.github.com>
2025-07-27 22:14:25 -0500

    Update header.ts

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
