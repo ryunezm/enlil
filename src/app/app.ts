b753b616993d14f5f2d4c4c2a655ab51ad26d808
Rubber Yunez <61928963+ryunezm@users.noreply.github.com>
2025-07-27 22:31:50 -0500

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Header} from "./header/header";
 import {Body} from "./body/body";
-import { Footer } from "./footer/footer";
+import {Footer} from "./footer/footer";
 
 @Component({
   selector: 'app-root',
